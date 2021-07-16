66274560fd96722aaf4d79f8d0b82eb031ec17f9
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-07-16 18:28:45 +0300

    Update KPSPublicSoap.java

--- northwind/src/main/java/tr/gov/nvi/tckimlik/Ws/KPSPublicSoap.java
+++ northwind/src/main/java/tr/gov/nvi/tckimlik/Ws/KPSPublicSoap.java
@@ -1,8 +1,13 @@
-package tr.gov.nvi.tckimlik.Ws;
+/**
+ * KPSPublicSoap.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
 
-public interface KPSPublicSoap {
+package tr.gov.nvi.tckimlik.WS;
 
-	public boolean TCKimlikNoDogrula(long TCKimlikNo, java.lang.String ad, java.lang.String soyad, int dogumYili) throws java.rmi.RemoteException;
-	
+public interface KPSPublicSoap extends java.rmi.Remote {
+    public boolean TCKimlikNoDogrula(long TCKimlikNo, java.lang.String ad, java.lang.String soyad, int dogumYili) throws java.rmi.RemoteException;
 }
 

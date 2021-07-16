b26cf779ce3492cf05da586c0ab9b64ca100edfd
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-07-16 18:27:04 +0300

    Update KPSPublic.java

--- northwind/src/main/java/tr/gov/nvi/tckimlik/Ws/KPSPublic.java
+++ northwind/src/main/java/tr/gov/nvi/tckimlik/Ws/KPSPublic.java
@@ -1,12 +1,16 @@
-package tr.gov.nvi.tckimlik.Ws;
+/**
+ * KPSPublic.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
 
-public interface KPSPublic {
+package tr.gov.nvi.tckimlik.WS;
 
-	    public java.lang.String getKPSPublicSoapAddress();
+public interface KPSPublic extends javax.xml.rpc.Service {
+    public java.lang.String getKPSPublicSoapAddress();
 
-	    public tr.gov.nvi.tckimlik.WS.KPSPublicSoap getKPSPublicSoap() throws javax.xml.rpc.ServiceException;
+    public tr.gov.nvi.tckimlik.WS.KPSPublicSoap getKPSPublicSoap() throws javax.xml.rpc.ServiceException;
 
-	    public tr.gov.nvi.tckimlik.WS.KPSPublicSoap getKPSPublicSoap(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
-	}
-
+    public tr.gov.nvi.tckimlik.WS.KPSPublicSoap getKPSPublicSoap(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
 }
